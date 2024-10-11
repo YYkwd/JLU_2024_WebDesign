2bb2d0a1960a8e78cf1352be48d00ebb114413a8
YutouXiansen <1536263456@qq.com>
2024-10-11 16:37:34 +0800

    正确的

--- vue_client/src/router/index.ts
+++ vue_client/src/router/index.ts
@@ -8,11 +8,34 @@ const router = createRouter({
             path:'/Administer', //管理员访问路径
             name:'Administer',
             component: () => import('../components/Administer.vue')
+            ,
+            /*children:[
+                {
+                    path:'/Administer/GoodsForSale', //管理员访问路径
+                    name:'AdministerGoodsForSale',
+                    component: () => import('../components/AdministerGoodsForSale.vue')
+                },
+                {
+                    path:'/Administer/Diliverymen', //管理员访问路径
+                    name:'AdministerDiliverymen',
+                    component: () => import('../components/AdministerDiliverymen.vue')
+                },
+                {
+                    path:'/Administer/Users', //管理员访问路径
+                    name:'AdministerUsers',
+                    component: () => import('../components/AdministerUsers.vue')
+                },
+                {
+                    path:'/Administer/Diliverymen', //管理员访问路径
+                    name:'AdministerGoods',
+                    component: () => import('../components/AdministerDiliverymen.vue')
+                },
+            ]*/
         },
         {
-            path:'/Home', 
+            path:'/', 
             name:'Home',
-            component: () => import('../App.vue')
+            component: () => import('../components/Home.vue')
         },
     ],
 })
